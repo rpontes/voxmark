b3f012f5ee368afb171fe519cb3f5f641d3d4212
Ricardo Pontes <rpontes@gmail.com>
2011-05-25 23:24:26 -0300

    Teste envio email

---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,12 @@
+ActionMailer::Base.smtp_settings = {
+
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => "gmail.com",
+  :user_name            => "rpontes@gmail.com",
+  :password             => "@nokia1106",
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+
+}
+
